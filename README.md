9aad7a2216b58ed40d6479eac84f25da8a05a6d9
zengfeng <77965984@qq.com>
2018-12-04 03:00:05 -0500

    first ci spring boot

---
+++ README.md
@@ -0,0 +1,10 @@
+
+Developing Your First Spring Boot Application
+
+Running the Example
+mvn spring-boot:run
+
+
+
+Creating an Executable Jar
+java -jar target/myproject-0.0.1-SNAPSHOT.jar
---
+++ src/main/java/Hello.java
@@ -0,0 +1,18 @@
+import org.springframework.boot.*;
+import org.springframework.boot.autoconfigure.*;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@EnableAutoConfiguration
+public class Hello{
+
+    @RequestMapping("/")
+    String home() {
+        return "Hello World!";
+    }
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(Hello.class, args);
+    }
+
+}
